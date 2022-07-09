f934a7af7a55853375a0488bbd30e256a2b38cae
IGOR-NaGIBIN <ignagibin2014@mail.ru>
2022-07-09 18:12:30 +0300

    вернул нормальный main

--- src/jvmMain/kotlin/main.kt
+++ src/jvmMain/kotlin/main.kt
@@ -1,25 +1,12 @@
-import logic.Algorithm
-import logic.GraphFileReader
+import application.Application
 import mu.KotlinLogging
+import org.apache.log4j.BasicConfigurator
 
 val logger = KotlinLogging.logger {}
 
 fun main() {
-//    BasicConfigurator.configure()
-//    val application = Application()
-//    application.start()
-    val gfr = GraphFileReader("info.txt")
-    val gfi = gfr.graphFromFile()
-    val alg = Algorithm()
-    val stateMachine = alg.dijkstraAlgorithm(gfi.graph, gfi.start)
-    val currentstep = if(gfi.stateNumber != null) gfi.stateNumber else 0
-    for (i in currentstep until stateMachine.size){
-        val state = stateMachine.getState(i)
-        println("Step $i current vertex is ${state!!.second}")
-        state.first.keys.forEach {
-            println("\t${it} - ${state.first[it]}")
-        }
-    }
-    val fileWriter = GraphFileWriter("name.txt")
-    fileWriter.toFile(gfi)
+    BasicConfigurator.configure()
+    val application = Application()
+    application.start()
+
 }
